82bc624e3049ec5a822c1dcca57d5639fa313703
Nathaniel Babalola <nathaniel.babalola1@gmail.com>
2020-12-11 02:48:28 +0100

    created an API endpoint (/analyze/tasks)

--- Readme.md
+++ Readme.md
@@ -1,3 +1,4 @@
+# For testing the function
 ## Sample of input
 Pass a string of this format into the analyzeTasks() function. <br>
 I would suggest using a template literal, and make sure there's no linebreak-<br>
@@ -20,4 +21,31 @@ or white-space at the beginning and at the end of the string
 2 10.00
 PT 7.50
 US 10.00
+```
+
+# For testing the API route
+
+## Sample of input
+
+On Postman select **Body** then select **raw** then select **text**.<br>
+Then post the below input in the text area.
+
+```
+2
+1 PT
+2 US
+3
+1 1 10
+2 1 5
+3 2 10
+```
+
+### To get the response in the standard output please the response **Body** option from **json** <br>
+### to **text**
+
+```
+2 10.00
+1 7.50
+US 10.00
+PT 7.50
 ```
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const { analyzeTasks } = require('./index');
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+//app.use(express.json());
+app.use(express.text({
+    defaultCharset: "utf-8"
+}));
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.get('/', (req, res) => {
+    res.status(200).send({
+        success: true,
+        message: 'Welcome to Task Analyzer 👷🏽‍♀️'
+    });
+});
+
+app.post('/analyze/tasks', async (req, res) => {
+    try {
+        const output = analyzeTasks(req.body);
+
+        if(!output){
+            res.status(404).send({
+                success: false
+            });
+        }
+
+        res.status(200);
+        res.format({
+            'text/plain' : function(){
+                res.send(output);
+            }
+        });
+    } catch (error) {
+        res.status(500).send();
+    }
+
+});
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port} 🌍`);
+});
